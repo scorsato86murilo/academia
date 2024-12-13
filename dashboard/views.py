7d64c7e8359b69ea9ab81dd4d6e90e042f37223d
scorsato86murilo <scorsato86murilo@gmail.com>
2024-12-13 18:32:13 -0300

    editar dash

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import CadastroAluno
 from django.contrib import messages
+
 
 def dashboard(request):
     aluno = None  # Variável para armazenar o aluno encontrado
@@ -24,6 +25,7 @@ def dashboard(request):
                 # Redirecionar para a página de dashboard com as mensagens
                 cpf = CadastroAluno.objects.all()
                 return render(request, 'dashboard.html', {'aluno_cpf': aluno_cpf, 'cpf': cpf})
+
         elif 'cadastrar' in request.POST:
             # Obter os dados do formulário de cadastro
             nome = request.POST.get('nome')
@@ -35,31 +37,38 @@ def dashboard(request):
             email = request.POST.get('email')
             id = request.POST.get('id')
 
-            if id:
-                pass
-
             # Verificar se o CPF já existe no banco de dados
-            if CadastroAluno.objects.filter(cpf=cpf).exists():
+            if CadastroAluno.objects.filter(cpf=cpf).exists() and not id:
                 messages.error(request, 'Erro: CPF já cadastrado!')
                 cpf = CadastroAluno.objects.all()
                 return render(request, 'dashboard.html', {'cpf': cpf})  # Retorna ao formulário com mensagem de erro
 
             try:
-                # Criar uma instância do modelo CadastroAluno
-                aluno = CadastroAluno(
-                    nome=nome,
-                    cpf=cpf,
-                    cidade=cidade,
-                    bairro=bairro,
-                    rua_av=rua_av,
-                    celular=celular,
-                    email=email,
-
-                )
-
-                # Salvar a instância no banco de dados
-                aluno.save()
-                messages.success(request, 'Cadastrado com SUCESSO!')
+                if id:
+                    # Atualizando o aluno existente
+                    aluno = get_object_or_404(CadastroAluno, id=id)
+                    aluno.nome = nome
+                    aluno.cpf = cpf
+                    aluno.cidade = cidade
+                    aluno.bairro = bairro
+                    aluno.rua_av = rua_av
+                    aluno.celular = celular
+                    aluno.email = email
+                    aluno.save()  # Salva as alterações
+                    messages.success(request, 'Aluno atualizado com SUCESSO!')
+                else:
+                    # Criar uma instância do modelo CadastroAluno
+                    aluno = CadastroAluno(
+                        nome=nome,
+                        cpf=cpf,
+                        cidade=cidade,
+                        bairro=bairro,
+                        rua_av=rua_av,
+                        celular=celular,
+                        email=email,
+                    )
+                    aluno.save()  # Salvar a instância no banco de dados
+                    messages.success(request, 'Cadastrado com SUCESSO!')
             except Exception as e:
                 messages.error(request, f'Erro: {str(e)}')
 
